a979ccbd2be53425a40d4fb2dc92ac391f53a692
graverealm <109730545+muzscripter@users.noreply.github.com>
2024-12-12 11:26:07 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -5,6 +5,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Queue = {};
 local Connections = {};
+local Networks = {};
 local ButNetScopes = (ReplicatedStorage['ButnetLibrary'] or Instance.new("Configuration", ReplicatedStorage));
 
 local function GetInstanceAttributes(Obj: Instance)
@@ -17,12 +18,18 @@ local function GetInstanceAttributes(Obj: Instance)
     end
 end
 
-local function ButnetSignalConnection(Name)
-    if not GetInstanceAttributes(ButNetScopes)[Name] or not Connections[Name] then
-
-    else
-
-    end
+local function ButnetSignalConnection(Name, Func)
+    task.defer(function()
+        if not GetInstanceAttributes(ButNetScopes)[Name] or not Connections[Name] then
+            ButNetScopes:SetAttribute(Name, nil)
+            Connections[Name] = ButNetScopes:GetAttributeChangedSignal(Name):Connect(function()
+                local AttributeValue = ButNetScopes:GetAttribute("Name")
+                --TODO:  Fire Func to all network scopes returning attribute value and original attribute
+            end)
+        else
+    
+        end
+    end)
 end
 
 function ButnetLibrary:CreateNetworkScope(NetworkName: string, Data)
@@ -38,3 +45,12 @@ function ButnetLibrary:CreateNetworkScope(NetworkName: string, Data)
 end
 
 return ButnetLibrary
+
+--// Example
+
+local ButnetLibrary = require(ReplicatedStorage.ButnetLibrary)
+
+local TimerScope = ButnetLibrary:CreateNetworkScope("Time", {
+    Func = function()
+    end
+})
