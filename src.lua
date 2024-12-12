3aa3716e1d220ce3d0a6348bc74a205e61e32ac4
graverealm <109730545+muzscripter@users.noreply.github.com>
2024-12-12 11:14:27 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -4,20 +4,34 @@ local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Queue = {};
-
+local Connections = {};
 local ButNetScopes = (ReplicatedStorage['ButnetLibrary'] or Instance.new("Configuration", ReplicatedStorage));
 
 local function GetInstanceAttributes(Obj: Instance)
-    
+    local AttributeList = Obj:GetAttributes()
+    if #AttributeList > 0 then
+        return AttributeList, true
+    else
+        warn("[ButnetLibrary]: Failed to fetch attributes for Instance" .. Obj.Name)
+        return nil, false 
+    end
 end
 
-function ButnetLibrary:CreateNetworkScope()
+local function ButnetSignalConnection(Name)
+    if not GetInstanceAttributes(ButNetScopes)[Name] or not Connections[Name] then
+
+    else
+
+    end
+end
+
+function ButnetLibrary:CreateNetworkScope(NetworkName: string, Data)
     local NetworkScope = {};
     NetworkScope.__index = NetworkScope
 
     function NetworkScope.new()
         local self = setmetatable({}, NetworkScope)
-        self.Connection = 
+        self.Connection = ButnetSignalConnection(NetworkName);
     end
 
     return NetworkScope.new()
